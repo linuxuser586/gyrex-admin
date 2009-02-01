1bf5b170ce4ae9fa93b57a64972efc9fee571410
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2009-02-01 14:57:46 +0000

    new field for passwords

---
+++ archive/toolkit/bundles/org.eclipse.gyrex.toolkit/src/org/eclipse/gyrex/toolkit/widgets/PasswordInput.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Gunnar Wagenknecht and others.
+ * All rights reserved.
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cloudfree.toolkit.widgets;
+
+/**
+ * A text dialog field specialized for password.
+ * <p>
+ * IMPORTANT: This class is intended to be subclassed <em>only</em> within the
+ * CWT implementation.
+ * </p>
+ */
+public class PasswordInput extends TextInput {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = -3131740758434715741L;
+
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param id
+	 *            the widget id
+	 * @param parent
+	 *            the widget parent
+	 * @param style
+	 *            the widget style
+	 */
+	public PasswordInput(final String id, final Container parent, final int style) {
+		super(id, parent, style);
+	}
+}
