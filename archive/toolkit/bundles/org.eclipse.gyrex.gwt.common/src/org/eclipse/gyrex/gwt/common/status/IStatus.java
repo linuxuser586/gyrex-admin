e9e69de7244b3c2ea6d6b66df4d8f060ba89ff57
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-07-13 10:16:57 +0000

    * remove GWT dependencies (Java Serializable is sufficient) * updated copyright headers

--- archive/toolkit/bundles/org.eclipse.gyrex.gwt.common/src/org/eclipse/gyrex/gwt/common/status/IStatus.java
+++ archive/toolkit/bundles/org.eclipse.gyrex.gwt.common/src/org/eclipse/gyrex/gwt/common/status/IStatus.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2008 Gunnar Wagenknecht and others.
+ * Copyright (c) 2008, 2009 Gunnar Wagenknecht and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
 package org.eclipse.gyrex.gwt.common.status;
 
-import com.google.gwt.user.client.rpc.IsSerializable;
+import java.io.Serializable;
 
 /**
  * A status object represents the outcome of an operation. Status objects are
@@ -43,7 +43,7 @@ import com.google.gwt.user.client.rpc.IsSerializable;
  * @see MultiStatus
  * @see Status
  */
-public interface IStatus extends IsSerializable {
+public interface IStatus extends Serializable {
 
 	/**
 	 * Status severity constant (value 0) indicating this status represents the
--- archive/toolkit/bundles/org.eclipse.gyrex.gwt.common/src/org/eclipse/gyrex/gwt/common/status/MultiStatus.java
+++ archive/toolkit/bundles/org.eclipse.gyrex.gwt.common/src/org/eclipse/gyrex/gwt/common/status/MultiStatus.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2008 Gunnar Wagenknecht and others.
+ * Copyright (c) 2008, 2009 Gunnar Wagenknecht and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
@@ -20,6 +20,9 @@ package org.eclipse.gyrex.gwt.common.status;
  * </p>
  */
 public class MultiStatus extends Status {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 6430029223661148877L;
 
 	/**
 	 * List of child statuses.
