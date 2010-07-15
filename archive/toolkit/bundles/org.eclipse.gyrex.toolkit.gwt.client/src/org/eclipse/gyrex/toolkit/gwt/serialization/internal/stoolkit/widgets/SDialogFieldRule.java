2071c4e8067fc05b01e6018230e7ff4daf779e64
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-07-15 11:39:48 +0000

    switched to serializable

--- archive/toolkit/bundles/org.eclipse.gyrex.toolkit.gwt.client/src/org/eclipse/gyrex/toolkit/gwt/serialization/internal/stoolkit/widgets/SDialogFieldRule.java
+++ archive/toolkit/bundles/org.eclipse.gyrex.toolkit.gwt.client/src/org/eclipse/gyrex/toolkit/gwt/serialization/internal/stoolkit/widgets/SDialogFieldRule.java
@@ -1,24 +1,26 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2009 Gunnar Wagenknecht and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
 package org.eclipse.gyrex.toolkit.gwt.serialization.internal.stoolkit.widgets;
 
+import java.io.Serializable;
 import java.util.Arrays;
-
-import com.google.gwt.user.client.rpc.IsSerializable;
 
 /**
  * Serializable DialogFieldRule
  */
-public class SDialogFieldRule implements IsSerializable {
+public class SDialogFieldRule implements Serializable {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 4255814565183312802L;
 
 	public static final int FST_SPECIFIED_FIELDS = 0;
 	public static final int FST_ALL_FIELDS = 1;
