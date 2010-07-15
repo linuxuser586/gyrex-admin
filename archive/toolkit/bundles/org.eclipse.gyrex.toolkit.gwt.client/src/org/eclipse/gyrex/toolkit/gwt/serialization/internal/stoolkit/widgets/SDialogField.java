a50c4e879ff9065afb5427c6d8bc19d3d5a8c194
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-07-15 11:30:26 +0000

    removed unused field

--- archive/toolkit/bundles/org.eclipse.gyrex.toolkit.gwt.client/src/org/eclipse/gyrex/toolkit/gwt/serialization/internal/stoolkit/widgets/SDialogField.java
+++ archive/toolkit/bundles/org.eclipse.gyrex.toolkit.gwt.client/src/org/eclipse/gyrex/toolkit/gwt/serialization/internal/stoolkit/widgets/SDialogField.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2009 Gunnar Wagenknecht and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
@@ -17,7 +17,6 @@ package org.eclipse.gyrex.toolkit.gwt.serialization.internal.stoolkit.widgets;
 public class SDialogField extends SWidget {
 	/** serialVersionUID */
 	private static final long serialVersionUID = -6355850982504897394L;
-	public String actionId;
 	public String label, description;
 	public boolean required, readOnly;
 	public SDialogFieldRule enablementRule;
