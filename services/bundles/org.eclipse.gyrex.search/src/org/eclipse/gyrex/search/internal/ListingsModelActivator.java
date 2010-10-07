17402aae89c9373680e6903748d37d539beccaa5
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-10-07 10:31:31 +0000

    updated BSN

--- services/bundles/org.eclipse.gyrex.search/src/org/eclipse/gyrex/search/internal/ListingsModelActivator.java
+++ services/bundles/org.eclipse.gyrex.search/src/org/eclipse/gyrex/search/internal/ListingsModelActivator.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2008 Gunnar Wagenknecht and others.
+ * Copyright (c) 2008, 2010 Gunnar Wagenknecht and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
  *******************************************************************************/
@@ -15,7 +15,7 @@ import org.eclipse.gyrex.common.runtime.BaseBundleActivator;
 
 public class ListingsModelActivator extends BaseBundleActivator {
 
-	private static final String SYMBOLIC_NAME = "org.eclipse.gyrex.cds.model";
+	private static final String SYMBOLIC_NAME = "org.eclipse.gyrex.cds";
 
 	/**
 	 * Creates a new instance.
