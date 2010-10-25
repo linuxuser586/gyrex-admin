42c6ab4c62ae166e2d6c3749513657c1dcaffad3
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-10-25 14:12:18 +0000

    fixed context tests and started implementing Solr CDS tests

---
+++ services/bundles/org.eclipse.gyrex.search.tests/src/org/eclipse/gyrex/search/solr/tests/AllSolrCdsTests.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2010 AGETO and others.
+ * All rights reserved.
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gyrex.cds.solr.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Context Test Suite.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ FacetManagerTest.class })
+public class AllSolrCdsTests {
+
+}
---
+++ services/bundles/org.eclipse.gyrex.search.tests/src/org/eclipse/gyrex/search/solr/tests/FacetManagerTest.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2010 Gunnar Wagenknecht and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ */
+package org.eclipse.gyrex.cds.solr.tests;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNotSame;
+import static junit.framework.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.eclipse.gyrex.cds.facets.IFacet;
+import org.eclipse.gyrex.cds.facets.IFacetManager;
+import org.eclipse.gyrex.cds.query.FacetSelectionStrategy;
+import org.eclipse.gyrex.cds.query.TermCombination;
+import org.eclipse.gyrex.cds.solr.ISolrCdsConstants;
+import org.eclipse.gyrex.context.tests.internal.BaseContextTest;
+import org.eclipse.gyrex.persistence.context.preferences.internal.ContextPreferencesRepositoryType;
+import org.eclipse.gyrex.persistence.internal.storage.DefaultRepositoryLookupStrategy;
+import org.eclipse.gyrex.persistence.storage.settings.IRepositoryPreferences;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+@SuppressWarnings("restriction")
+public class FacetManagerTest extends BaseContextTest {
+
+	/** TEST_FACET */
+	private static final String TEST_FACET = "Test Facet";
+	/** NAME */
+	private static final String REPOSITORY_ID = FacetManagerTest.class.getName();
+
+	@Override
+	protected IPath getPrimaryTestContextPath() {
+		return new Path("/__internal/org/eclipse/gyrex/cds/solr/tests");
+	}
+
+	@Override
+	protected void initContext() throws Exception {
+		DefaultRepositoryLookupStrategy.setRepository(getContext(), ISolrCdsConstants.FACET_CONTENT_TYPE, REPOSITORY_ID);
+		IRepositoryPreferences preferences;
+		try {
+			preferences = SolrCdsTestsActivator.getInstance().getRepositoryRegistry().createRepository(REPOSITORY_ID, ContextPreferencesRepositoryType.ID);
+		} catch (final IllegalStateException e) {
+			// assume already exist
+			preferences = SolrCdsTestsActivator.getInstance().getRepositoryRegistry().getRepositoryPreferences(REPOSITORY_ID);
+		}
+		assertNotNull(preferences);
+		preferences.getPreferences().put(ContextPreferencesRepositoryType.PREF_KEY_CONTEXT_PATH, getContext().getContextPath().toString());
+		preferences.flush();
+	}
+
+	@Test
+	public void test001_ManagerBasics() throws Exception {
+		final IFacetManager manager = getContext().get(IFacetManager.class);
+		assertNotNull(manager);
+
+		// try to remove any existing facets
+		final Map<String, IFacet> existingFacets = manager.getFacets();
+		assertNotNull(existingFacets);
+		if (!existingFacets.isEmpty()) {
+			for (final IFacet facet : existingFacets.values()) {
+				manager.delete(facet);
+				assertNotNull(manager.getFacets());
+				assertFalse(manager.getFacets().containsKey(facet.getAttributeId()));
+			}
+		}
+
+		// check empty
+		assertNotNull(manager.getFacets());
+		assertTrue(manager.getFacets().isEmpty());
+
+		// create transient facet
+		final IFacet facet = manager.create("test");
+		assertNotNull(facet);
+
+		// must still be empty
+		assertNotNull(manager.getFacets());
+		assertFalse(manager.getFacets().containsKey(facet.getAttributeId()));
+
+		// now save facet
+		manager.save(facet);
+
+		// must not be empty anymore
+		assertNotNull(manager.getFacets());
+		assertEquals(1, manager.getFacets().size());
+		assertTrue(manager.getFacets().containsKey(facet.getAttributeId()));
+	}
+
+	@Test
+	public void test002_FacetPersistence() throws Exception {
+		final IFacetManager manager = getContext().get(IFacetManager.class);
+		assertNotNull(manager);
+
+		// create facet
+		final IFacet facet = manager.create("test");
+		assertNotNull(facet);
+
+		facet.setName(TEST_FACET);
+		assertEquals(facet.getName(), TEST_FACET);
+
+		facet.setSelectionStrategy(FacetSelectionStrategy.MULTI);
+		assertEquals(facet.getSelectionStrategy(), FacetSelectionStrategy.MULTI);
+
+		facet.setTermCombination(TermCombination.AND);
+		assertEquals(facet.getTermCombination(), TermCombination.AND);
+
+		// now save facet
+		manager.save(facet);
+
+		// re-load
+		final IFacet saveFacet = manager.getFacets().get(facet.getAttributeId());
+		assertNotNull(saveFacet);
+
+		// must be different object
+		assertNotSame(facet, saveFacet);
+
+		// check properties
+		assertEquals(facet.getName(), saveFacet.getName());
+		assertEquals(facet.getSelectionStrategy(), saveFacet.getSelectionStrategy());
+		assertEquals(facet.getTermCombination(), saveFacet.getTermCombination());
+	}
+}
